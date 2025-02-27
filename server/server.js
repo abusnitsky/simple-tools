614280192bd70e76e39e60e8b1c07a963bf8da24
abusnitsky <abusnitsky@gmail.com>
2025-02-27 19:50:07 +0100

    add env

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-//import express from 'express';
-//import cors from 'cors';
+import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import Todo from './model/Todo.js';
-//const app = express();
+const app = express();
 const uri = "mongodb+srv://abusnitsky:devpass@simpletools.ngo3l.mongodb.net/?retryWrites=true&w=majority&appName=SimpleTools";
 
-/* const corsOptions = {
+const corsOptions = {
     origin: 'http://localhost:5173',
-}; */
+};
 
 mongoose.connect(uri);
-/* app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 
 app.get('/test', (req, res) => {
@@ -19,8 +19,5 @@ app.get('/test', (req, res) => {
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
-}); */
+});
 
-const todo = await Todo.where("done").equals(false);
-
-console.log(todo);
