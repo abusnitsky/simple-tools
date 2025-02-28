f3fdff4fda50bd18d0d5895c8bad4fb8e1650657
abusnitsky <abusnitsky@gmail.com>
2025-02-28 15:41:20 +0100

    add todo rest to server

--- server/model/Todo.js
+++ server/model/Todo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const todoSchema = new Schema({
-    description: { type: String, required: true },
+    text: { type: String, required: true },
     done: { type: Boolean, required: true },
     created: { type: Date, default: Date.now },
 });
--- server/server.js
+++ server/server.js
@@ -1,23 +1,37 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import Todo from './model/Todo.js';
+
 const app = express();
-const uri = "mongodb+srv://abusnitsky:devpass@simpletools.ngo3l.mongodb.net/?retryWrites=true&w=majority&appName=SimpleTools";
+const uri = "mongodb+srv://abusnitsky:devpass@simpletools.ngo3l.mongodb.net/simptdb?retryWrites=true&w=majority&appName=SimpleTools";
 
-const corsOptions = {
+/* const corsOptions = {
     origin: 'http://localhost:5173',
-};
+}; */
 
-mongoose.connect(uri);
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(express.json());
 
+mongoose.connect(uri)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
 
-app.get('/test', (req, res) => {
-    res.send('Hello World');
+app.post("/todos", async (req, res) => {
+    try {
+      const newTodo = await Todo.create(req.body);
+      res.status(201).json(newTodo);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to add item" });
+    }
+  });
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.send(todos);
 });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-});
+app.listen(8080, () => console.log('Server is running on port 8080'));
 
